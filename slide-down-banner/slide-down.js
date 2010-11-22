ad4c2de34e643c7cd1d66433fca38709e43502c6
RainoXu <tblonggang@gmail.com>
2010-11-22 09:16:01 +0800

    new idea of slide down banner

---
+++ slide-down-banner/slide-down.js
@@ -0,0 +1,15 @@
+KISSY.add('slide-down-banner', function (S, undefined){
+	var S = KISSY, Dom = S.DOM, Event = S.Event;
+
+	function SlideDownBanner(container, config){
+		if (){
+		}
+	}
+	
+	S.augment(SlideDownBanner, S.EventTarget, function (){
+
+	});
+
+	S.SlideDownBanner = SlideDownBanner;
+
+});
